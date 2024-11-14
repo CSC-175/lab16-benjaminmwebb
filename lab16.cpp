83db004ab3a4e3aa70547052360b351f1764bfe8
benjaminmwebb <benmwebb@umich.edu>
2024-11-14 17:14:49 -0500

    Update lab16.cpp

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,12 @@
-// Code to implement the gcf function goes here
+#include <iostream>
+using namespace std;
+
+// Function to calculate the greatest common factor (GCF) or greatest common divisor (GCD)
+int gcf(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;  // a % b gives the remainder when a is divided by b
+        a = temp;
+    }
+    return a;  // When b becomes 0, a contains the GCF
+}
